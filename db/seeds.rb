d55e09957c47e5d2a357b2d6b1bbc10870eab357
Jason Dancocks <jason.dancocks@gmail.com>
2017-09-07 21:59:25 +0100

    updated seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
-10.times do
+30.times do
 	@name = Faker::GameOfThrones.character
 	user = User.create(name: @name, email: Faker::Internet.email(@name.gsub(/\s+/, ".")), password: "password")
 	rand(1..5).times do 
@@ -15,4 +15,17 @@ User.all.each do |user|
 		user.comments.create(post_id: post_id, body: Faker::Simpsons.quote)
 	end
 end
+
+admin = User.create(name: "admin", email:"admin@example.com", password: "password")
+admin.save
+
+i = 0
+while i < User.all.count
+@friendship = admin.friendships.create(friend_id: i)
+@friendship.update(accepted: true)
+@friendship.save
+
+i+= 1
+end
+
 
